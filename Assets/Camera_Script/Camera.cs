af1fc66d55c9fbb4c6b7e960a7d6d90727b9a5e5
kusanagi <ken0704shin@gmail.com>
2026-05-18 00:56:59 +0900

    カメラ追従

---
+++ Assets/Camera_Script/Camera.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    //対象の位置取得
+    [SerializeField] private Transform target;
+    //移動範囲限度値設定
+    [SerializeField] private float xmin;
+    [SerializeField] private float xmax;
+    [SerializeField] private float ymin;
+    [SerializeField] private float ymax;
+
+    void Update()
+    {
+        FollowTarget();
+    }
+    //カメラの位置に対象の位置を代入
+    private void FollowTarget()
+    {
+        if (target != null)
+        {
+            float x = Mathf.Clamp(target.position.x, xmin, xmax);
+            float y = Mathf.Clamp(target.position.y, ymin, ymax);
+            transform.position = new Vector3(x, y, transform.position.z);
+        }
+    }
+}
